50d32f66f566bd0522974ca31ca7aa52e2fd0a9b
Reeth Dasgupta <115037176+reethd@users.noreply.github.com>
2023-03-30 20:23:45 -0400

    created User model

---
+++ server/models/User.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  events: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Event",
+    },
+  ],
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model("User", userSchema);
+
+module.exports = User;
