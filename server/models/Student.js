7c160d454d97bd9587d8124550fb49a458220414
Reeth Dasgupta <115037176+reethd@users.noreply.github.com>
2023-04-04 16:19:24 -0400

    worked on seeds

--- server/models/Student.js
+++ server/models/Student.js
@@ -1,28 +1,26 @@
 const { Schema, model } = require("mongoose");
 
 const studentSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-      },
-      email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/.+@.+\..+/, "Must match an email address!"],
-      },
-      phone: {
-        type: String,
-        required: true,
-        // match: [/^[\.-)( ]*([0-9]{3})[\.-)( ]*([0-9]{3})[\.-)( ]*([0-9]{4})$/,"Must be a phone number!",],
-      },
-      event: {
-        type: Schema.Types.ObjectId,
-        ref: 'Event'
-      }
-
-})
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+  phone: {
+    type: String,
+    required: true,
+    // match: [/^[\.-)( ]*([0-9]{3})[\.-)( ]*([0-9]{3})[\.-)( ]*([0-9]{4})$/,"Must be a phone number!",],
+  },
+  event: {
+    type: Schema.Types.ObjectId,
+    ref: "Event",
+  },
+});
 
 // const Student = model("Student", studentSchema);
 
---
+++ server/seeders/seed.js
@@ -0,0 +1,28 @@
+const db = require("../config/connection");
+const { User, Event } = require("../models");
+
+const userData = require("./userData.json");
+const eventData = require("./eventData.json");
+const studentData = require("./studentData.json");
+
+db.once("open", async () => {
+  await User.deleteMany({});
+  await Event.deleteMany({});
+
+  const users = await User.insertMany(userData);
+  const events = await Event.insertMany(eventData);
+
+  //   for (newEvent of events) {
+  //     // randomly add each event to a user
+  //     const tempUser = users[Math.floor(Math.random() * users.length)];
+  //     tempUser.events.push(newEvent._id);
+  //     // assigns teacher to an event
+  //     newEvent.teacher = tempUser._id;
+
+  //     await newEvent.save();
+  //     await tempUser.save();
+  //   }
+
+  console.log("Holy fuck it worked!");
+  process.exit(0);
+});
