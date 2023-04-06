4374c6fcbc5d9c9d3a20b275dd9e9d23f910cc35
Reeth Dasgupta <115037176+reethd@users.noreply.github.com>
2023-04-06 17:25:04 -0400

    added queries and mutations to client utils

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,99 @@
+import { gql } from "@apollo/client";
+
+export const LOG_IN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+      user
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user
+    }
+  }
+`;
+
+export const ADD_EVENT = gql`
+  mutation addEvent($newEvent: InputEvent!) {
+    addEvent(newEvent: $newEvent) {
+      username
+      events
+    }
+  }
+`;
+
+export const ADD_STUDENT = gql`
+  mutation addStudent($newStudent: InputStudent!) {
+    addStudent(newStudent: $newStudent) {
+      title
+      students
+    }
+  }
+`;
+
+export const REMOVE_EVENT = gql`
+  mutation removeEvent($_id: ID!) {
+    removeEvent(_id: $_id) {
+      username
+      events
+    }
+  }
+`;
+
+export const REMOVE_STUDENT = gql`
+  mutation removeStudent($_id: ID!) {
+    removeStudent(_id: $_id) {
+      title
+      students
+    }
+  }
+`;
+
+export const UPDATE_EVENT_TITLE = gql`
+  mutation updateEventTitle($_id: ID!, $title: String!)
+    updateEventTitle(_id: $_id, title: $title) {
+        title
+    }
+`;
+
+export const UPDATE_EVENT_LOCATION = gql`
+mutation updateEventLocation($_id: ID!, $location: String!)
+    updateEventLocation(_id: $_id, location: $location) {
+        title
+        location
+    }`;
+
+export const UPDATE_EVENT_DATE = gql`
+mutation updateEventDate($_id: ID!, $date: String!)
+    updateEventDate(_id: $_id, date: $date) {
+        title
+        date
+        time
+    }`;
+
+export const UPDATE_EVENT_TIME = gql`
+mutation updateEventTime($_id: ID!, $time: String!)
+    updateEventTime(_id: $_id, time: $time) {
+        title
+        date
+        time
+    }`;
+
+export const UPDATE_EVENT_IMAGESOURCE = gql`
+mutation updateEventImageSource($_id: ID!, $imageSource: String!)
+    updateEventImageSource(_id: $_id, imageSource: $imageSource) {
+        title
+        imageSource
+    }`;
+
+export const UPDATE_EVENT_DESCRIPTION = gql`
+mutation updateEventDescription($_id: ID!, $description: String!)
+    updateEventDescription(_id: $_id, description: $description) {
+        title
+        description
+    }`;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,53 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_USERS = gql`
+  query allUsers {
+    users {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  query singleUser($_id: ID!) {
+    user(_id: $_id) {
+      _id
+      username
+      email
+      events
+    }
+  }
+`;
+
+export const QUERY_EVENTS = gql`
+  query allEvents {
+    events {
+      createdAt
+      title
+      teacher
+      location
+      date
+      time
+      imageSource
+      description
+    }
+  }
+`;
+
+export const QUERY_EVENT = gql`
+  query singleEvent(_id: $_id) {
+    event(_id: $_id) {
+      createdAt
+      title
+      teacher
+      location
+      date
+      time
+      imageSource
+      description
+      students
+    }
+  }
+`;
