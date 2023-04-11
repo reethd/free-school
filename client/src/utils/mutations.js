0ec5d7a45050a947bccf27a913d333af0dbaaac3
Kyle Franklin <kpfranklin22@gmail.com>
2023-04-10 20:56:50 -0400

    graphql mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,47 +1,66 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-mutation Login($username: String!, $password: String!) {
-  login(username: $username, password: $password) {
-    user {
-      username
-      password
+  mutation Login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      user {
+        username
+        password
+      }
+      token
     }
-    token
   }
-}
 `;
 
 export const ADD_USER = gql`
-mutation AddUser($username: String!, $password: String!, $email: String!) {
-  addUser(username: $username, password: $password, email: $email) {
-    token
+  mutation AddUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email: $email) {
+      token
+    }
   }
-}
 `;
 
 export const ADD_EVENT = gql`
-mutation AddEvent($title: String!, $location: String!, $date: String!, $time: String!, $description: String!, $imageSource: String) {
-  addEvent(title: $title, location: $location, date: $date, time: $time, description: $description, imageSource: $imageSource) {
-    title
+  mutation AddEvent(
+    $title: String!
+    $location: String!
+    $date: String!
+    $time: String!
+    $description: String!
+    $imageSource: String
+  ) {
+    addEvent(
+      title: $title
+      location: $location
+      date: $date
+      time: $time
+      description: $description
+      imageSource: $imageSource
+    ) {
+      title
+    }
   }
-}
 `;
 
 export const ADD_STUDENT = gql`
-  mutation addStudent($newStudent: InputStudent!) {
-    addStudent(newStudent: $newStudent) {
-      title
-      students
+  mutation AddStudent($event: ID!, $newStudent: InputStudent) {
+    addStudent(event: $event, newStudent: $newStudent) {
+      students {
+        name
+        phone
+        email
+      }
     }
   }
 `;
 
 export const REMOVE_EVENT = gql`
-  mutation removeEvent($_id: ID!) {
-    removeEvent(_id: $_id) {
-      username
-      events
+  mutation RemoveEvent($id: ID!) {
+    removeEvent(_id: $id) {
+      _id
+      events {
+        title
+      }
     }
   }
 `;
@@ -49,8 +68,10 @@ export const REMOVE_EVENT = gql`
 export const REMOVE_STUDENT = gql`
   mutation removeStudent($_id: ID!) {
     removeStudent(_id: $_id) {
-      title
-      students
+      _id
+      students{
+        name
+      }
     }
   }
 `;
@@ -58,49 +79,52 @@ export const REMOVE_STUDENT = gql`
 export const UPDATE_EVENT_TITLE = gql`
   mutation updateEventTitle($_id: ID!, $title: String!) {
     updateEventTitle(_id: $_id, title: $title) {
-        title
+      title
     }
   }
 `;
 
 export const UPDATE_EVENT_LOCATION = gql`
-mutation updateEventLocation($_id: ID!, $location: String!) {
-    updateEventLocation(_id: $_id, location: $location) {
-        title
-        location
+  mutation UpdateEventLocation($id: ID!, $location: String) {
+    updateEventLocation(_id: $id, location: $location) {
+      _id
+      location
     }
-  }`;
+  }
+`;
 
 export const UPDATE_EVENT_DATE = gql`
-mutation updateEventDate($_id: ID!, $date: String!) {
-    updateEventDate(_id: $_id, date: $date) {
-        title
-        date
-        time
+  mutation UpdateEventDate($id: ID!, $date: String) {
+    updateEventDate(_id: $id, date: $date) {
+      _id
+      date
     }
-  }`;
+  }
+`;
 
 export const UPDATE_EVENT_TIME = gql`
-mutation updateEventTime($_id: ID!, $time: String!) {
-    updateEventTime(_id: $_id, time: $time) {
-        title
-        date
-        time
+  mutation UpdateEventTime($id: ID!, $title: String) {
+    updateEventTitle(_id: $id, title: $title) {
+      _id
+      title
     }
-  }`;
+  }
+`;
 
 export const UPDATE_EVENT_IMAGESOURCE = gql`
-mutation updateEventImageSource($_id: ID!, $imageSource: String!) {
-    updateEventImageSource(_id: $_id, imageSource: $imageSource) {
-        title
-        imageSource
+  mutation UpdateEventDate($id: ID!, $imageSource: String) {
+    updateEventImageSource(_id: $id, imageSource: $imageSource) {
+      _id
+      imageSource
     }
-  }`;
+  }
+`;
 
 export const UPDATE_EVENT_DESCRIPTION = gql`
-mutation updateEventDescription($_id: ID!, $description: String!) {
-    updateEventDescription(_id: $_id, description: $description) {
-        title
-        description
+  mutation UpdateEventDate($id: ID!, $description: String) {
+    updateEventDescription(_id: $id, description: $description) {
+      _id
+      description
     }
-  }`;
+  }
+`;
