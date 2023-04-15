b70a2d79d0d60db7cd3f19de892a0f98f948f1d3
Reeth Dasgupta <115037176+reethd@users.noreply.github.com>
2023-04-14 20:24:52 -0400

    deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,8 +5,6 @@ mongoose.connect(
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/free-school",
   {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
     useUnifiedTopology: true,
   }
 );
